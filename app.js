2141d098db121961067726d02e73ae6e9f94b980
ravim95 <ravim.95@gmail.com>
2020-08-13 03:25:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.get('/register', (req, res, next) => {
   })
 })
 //listen on port 3000
-app.listen(3001, () => {
- console.log('Server listening on port 3001')
+app.listen(3000, () => {
+ console.log('Server listening on port 3000')
 })
